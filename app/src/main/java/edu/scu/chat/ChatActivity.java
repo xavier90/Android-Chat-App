0de94a350462daa310bc9a79fe076b9b42fe4b79
Yaojian Wang <yaojianwang@Yaojians-MacBook-Pro.local>
2017-06-10 00:59:48 -0700

    implement edit profile

--- app/src/main/java/edu/scu/chat/ChatActivity.java
+++ app/src/main/java/edu/scu/chat/ChatActivity.java
@@ -1,10 +1,13 @@
 package edu.scu.chat;
 
-/**
- * Created by yaojianwang on 6/7/17.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class ChatActivity extends BaseActivity{
+public class ChatActivity extends AppCompatActivity {
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
 }
